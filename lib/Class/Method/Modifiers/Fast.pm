e8f967b40a2b631dc31e417aca6de37a81b80e22
gfx <gfuji@cpan.org>
2009-12-24 16:41:42 +0900

    Checking in changes prior to tagging of version 0.02.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 9347104..0d6dc4e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,7 +1,8 @@
     Revision history for Perl extension Class::Method::Modifiers::Fast
    
    -0.02
    +0.02 Thu Dec 24 16:40:37 2009
         * Add install_modifier as a public API (gfx)
    +    * Require Data::Util 0.55 explicitly (gfx)
    
     0.01 Sun Jan 18 17:19:17 2009
     	* original version

--- lib/Class/Method/Modifiers/Fast.pm
+++ lib/Class/Method/Modifiers/Fast.pm
@@ -2,7 +2,7 @@ package Class::Method::Modifiers::Fast;
 use strict;
 use warnings;
 use Data::Util;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use base 'Exporter';
 our @EXPORT      = qw(before after around);
